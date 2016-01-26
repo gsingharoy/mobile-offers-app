3bc842488e00c25a682da1d09492267cf8c771ac
gsingharoy <neogauravsvnit@gmail.com>
2016-01-26 23:11:48 +0100

    added validations in the form

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,7 +5,11 @@ class OffersController < ApplicationController
     if result.success?
       @offers = result.offers
     else
-
+      if result.error == 'API_INTERNAL_ERROR'
+        @errors = ['There was an error in the Fyber API. Try again later.']
+      else
+        @errors = [result.message]
+      end
     end
   end
 
