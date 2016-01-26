f8484e78b26913346ce4959c83dc398521f09974
gsingharoy <neogauravsvnit@gmail.com>
2016-01-26 23:04:55 +0100

    created a search page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,12 @@
 class OffersController < ApplicationController
 
   def search
+    result = RetrieveOffers.call(pub0: params[:pub0], uid: params[:uid], page: params[:page])
+    if result.success?
+      @offers = result.offers
+    else
 
+    end
   end
 
   def new_search
